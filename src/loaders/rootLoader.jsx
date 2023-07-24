85e349855925dbe75b2e046075b02f0d236e857c
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-07-24 20:02:52 +0100

    add auth to loaders

--- src/loaders/rootLoader.jsx
+++ src/loaders/rootLoader.jsx
@@ -1,7 +1,14 @@
+import { redirect } from "react-router-dom"
+
 export default async function rootLoader() {
     const response = await fetch("http://localhost:3000")
 
-    const data = await response.json()
-
-    return data
+    if(!response.ok) {
+        return redirect('/signup')
+    }
+    else {
+        const data = await response.json()
+        
+        return data
+    }
 }
