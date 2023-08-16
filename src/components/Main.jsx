2f4b1d1408562d1c926e5000cb09873b2793256a
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-08-16 19:04:27 +0100

    remove some listings

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,28 +4,8 @@ import Slider from "./Slider";
 import ShowButton from "./ShowButton";
 import TextSlider from "./TextSlider";
 
-import image1 from "../assets/bnb1.jpg"
-import image2 from "../assets/bnb2.jpg"
-
 export default function Main() {
-    const houses = [
-        {
-            image: image1,
-            location: "Ngaparou, Senegal",
-            distance: "2,403 kilometers away",
-            days: "Sep 4 - 10",
-            price: 152.00,
-            rating: 4.86
-        },
-        {
-            image: image2,
-            location: "taghazout, Morocco",
-            distance: "2,403 kilometers away",
-            days: "Aug 15 - 20",
-            price: 373.00,
-            rating: 4.82
-        },
-    ]
+    const houses = []
     
     const info = {
         heading: "Inspiration for future getaways",
