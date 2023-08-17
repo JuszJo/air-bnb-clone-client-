1d6d6fe9f4b20341cc418d933c0026be37187a62
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-08-17 02:17:52 +0100

    make listing better

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -20,8 +20,41 @@ export default function Listing() {
                 { 
                     houseData &&
                         <div>
-                            <div className="container">
-                                <h3>{houseData.name}</h3>
+                            <div className="listing-container">
+                                <div>
+                                    <p>{houseData.owner}</p>
+                                </div>
+                                <div>
+                                    <img src={houseData.images[0]} alt="listing" className="listing-img" />
+                                </div>
+                                <div>
+                                    <div>
+                                        <div>
+                                            <div>
+                                                <h2>{houseData.name}</h2>
+                                            </div>
+                                            <div>
+                                                <p>{houseData.price}</p>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <p>{houseData.location}</p>
+                                        </div>
+                                        <div>
+                                            <p>{houseData.rating}</p>
+                                        </div>
+                                        <div>
+                                            <article>
+                                                {houseData.description}
+                                            </article>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <article>
+                                            {/* {houseData.reviews[0]} */}
+                                        </article>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                 }
