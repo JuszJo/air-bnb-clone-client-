0717611b8e4b53283e95dd4be0c2b9de4a9e1d09
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-08-17 01:58:00 +0100

    consume api for listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import Navbar from "../components/Navbar"
 
 export default function Listing() {
+    const [houseData, setHouseDate] = useState(null)
+
     const params = useParams()
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/listing/${params.id}`)
+            .then(response => response.json())
+            .then(data => setHouseDate(data))
+    }, [])
     
     return (
         <>
-            <h1>Listing</h1>
+            <section>
+                <Navbar />
+                { 
+                    houseData &&
+                        <div>
+                            <div className="container">
+                                <h3>{houseData.name}</h3>
+                            </div>
+                        </div>
+                }
+            </section>
         </>
     )
 }
