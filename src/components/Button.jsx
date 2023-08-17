164ee452bfa6dbbb3151e8dc29bb74839a98cd74
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-08-17 09:05:05 +0100

    add css to listing page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-const primaryButtonStyle = {
+let primaryButtonStyle = {
     backgroundColor: "var(--pink)",
     border: "none",
     padding: "6px 14px",
@@ -7,7 +7,7 @@ const primaryButtonStyle = {
     borderRadius: "4px"
 }
 
-const secondaryButtonStyle = {
+let secondaryButtonStyle = {
     backgroundColor: "transparent",
     border: "1px solid var(--pink)",
     padding: "6px 14px",
@@ -17,7 +17,19 @@ const secondaryButtonStyle = {
     marginLeft: "6px"
 }
 
-export default function Button({text, primary}) {
+export default function Button({text, primary, style}) {
+    if(style) {
+        primaryButtonStyle = {
+            ...primaryButtonStyle,
+            ...style
+        }
+
+        secondaryButtonStyle = {
+            ...primaryButtonStyle,
+            ...style
+        }
+    }
+
     return (
         <>
             <button style={primary ? primaryButtonStyle : secondaryButtonStyle}>{text}</button>
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Navbar from "../components/Navbar"
 
+import ratings from "../assets/ratings.png"
+import Button from "../components/Button"
+
 export default function Listing() {
     const [houseData, setHouseDate] = useState(null)
 
@@ -21,36 +24,41 @@ export default function Listing() {
                     houseData &&
                         <div>
                             <div className="listing-container">
-                                <div>
+                                <div id="house-owner-div">
                                     <p>{houseData.owner}</p>
                                 </div>
-                                <div>
+                                <div id="house-image-div">
                                     <img src={houseData.images[0]} alt="listing" className="listing-img" />
                                 </div>
-                                <div>
-                                    <div>
-                                        <div>
-                                            <div>
-                                                <h2>{houseData.name}</h2>
+                                <div id="house-info-div">
+                                    <div id="house-text-div">
+                                        <div id="house-identity-div">
+                                            <div id="house-name-div">
+                                                <h2 id="house-name">{houseData.name}</h2>
                                             </div>
-                                            <div>
-                                                <p>{houseData.price}</p>
+                                            <div id="house-price-div">
+                                                <p id="house-price">${houseData.price}<span>per night</span></p>
                                             </div>
                                         </div>
-                                        <div>
-                                            <p>{houseData.location}</p>
+                                        <div id="house-location-div">
+                                            <p id="house-location">{houseData.location}</p>
                                         </div>
-                                        <div>
-                                            <p>{houseData.rating}</p>
+                                        <div id="house-rating-div">
+                                            {/* <p id="house-rating">{houseData.rating}</p> */}
+                                            <img src={ratings} alt="ratings" id="house-rating" />
                                         </div>
-                                        <div>
-                                            <article>
+                                        <hr style={{border: "0.5px solid var(--light-grey)", marginTop: "8px"}} />
+                                        <div id="house-description-div">
+                                            <article id="house-description">
                                                 {houseData.description}
                                             </article>
                                         </div>
+                                        <div id="reserve-button-div">
+                                            <Button text={"Reserve"} primary style={{width: "100%"}} />
+                                        </div>
                                     </div>
-                                    <div>
-                                        <article>
+                                    <div id="house-review-div">
+                                        <article id="house-review">
                                             {/* {houseData.reviews[0]} */}
                                         </article>
                                     </div>
