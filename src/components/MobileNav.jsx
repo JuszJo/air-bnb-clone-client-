1e8636672ba922e95fec510c87271c149c542c37
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2023-08-18 09:33:25 +0100

    nav to login from mobile

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,13 +1,20 @@
+import { useNavigate } from "react-router-dom"
 import loginIcon from "../assets/login.svg"
 
 export default function MobileNav() {
+    const navigate = useNavigate()
+
+    function handleClick() {
+        navigate("/login")
+    }
+
     return (
         <>
             <section id="mobile-section">
                 <div>
                     <div className="container">
                         <div>
-                            <img width={32} src={loginIcon} />
+                            <img onClick={handleClick} width={32} src={loginIcon} />
                         </div>
                     </div>
                 </div>
